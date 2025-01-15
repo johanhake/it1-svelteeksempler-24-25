86f5f10f716ac86f650221c09c024ff78e37aea1
johanhake <johan.hake@gmail.com>
2025-01-15 10:34:34 +0100

    Oppdatering fra timen

--- src/routes/kapittel_5/5.2.2_regnemesteren/+page.svelte
+++ src/routes/kapittel_5/5.2.2_regnemesteren/+page.svelte
@@ -1,1 +1,13 @@
+<script>
+    let tall1 = 45
+    let tall2 = 3.4
+    let tekst = "jada"
+</script>
 <h1>Regnemesteren</h1>
+<p>{tall1} + {tall2} = {tall1+tall2}</p>
+<p>{tall1} * {tall2} = {tall1*tall2}</p>
+<p>Tall og tekst kan ikke ganges: {tall1*tekst}</p>
+<p>Datatypen til tall: {typeof tall1}</p>
+<p>Datatypen til tekst: {typeof tekst}</p>
+<p>Datatypen til NaN: {typeof NaN}</p>
+<p>Datatypen til undefined: {typeof undefined}</p>
