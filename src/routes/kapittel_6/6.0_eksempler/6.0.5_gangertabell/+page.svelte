501706cc59c07f6a192e96ca1aba1e5efc86ce7a
johanhake <johan.hake@gmail.com>
2025-02-24 15:26:19 +0100

    La til gangertabellen

--- src/routes/kapittel_6/6.0_eksempler/6.0.5_gangertabell/+page.svelte
+++ src/routes/kapittel_6/6.0_eksempler/6.0.5_gangertabell/+page.svelte
@@ -2,13 +2,32 @@
 	// @ts-nocheck
 
 	let ganger_tall = [1, 2, 3, 4, 5];
+	let antall = 5
+
+	const oppdater_tabell= ()=>{
+		ganger_tall = []
+		for (let i=1;i<=antall;i++){
+			ganger_tall.push(i)
+		}
+	}
 </script>
 
 <h2>Gangertabell:</h2>
+<input type="number" bind:value={antall} on:change={oppdater_tabell}>
 
-<div class="tabell" style="">
+<div
+	class="tabell"
+	style="grid-template-columns: repeat({ganger_tall.length+1}, 1fr)"
+>
+	<div></div>
 	{#each ganger_tall as tall}
 		<div class="topp">{tall}</div>
+	{/each}
+	{#each ganger_tall as tall_1}
+		<div class="venstre">{tall_1}</div>
+		{#each ganger_tall as tall_2}
+			<div>{tall_1*tall_2}</div>
+		{/each}
 	{/each}
 </div>
 
@@ -16,7 +35,6 @@
 	.tabell {
 		display: grid;
 		width: 600px;
-		grid-template-columns: repeat(6, 1fr);
 	}
 
 	div {
